40f039c22e551861a0f6a02ffd47ecb3fcb0ba55
Khesav Chhatra <shahkhesavchhatra@gmail.com>
2022-01-25 17:05:57 +0545

    Intial Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import {CssBaseline,Grid} from '@material-ui/core';
+
+import Header from './components/Header/Header';
+import Map from "./components/Map/Map";
+//import PlaceDetails from "./component/PlaceDetails/PlaceDetails";
+import List from "./components/List/List";
 const App=()=>{
 	return (
-		<div>
-		<h1>Hello world :)</h1>
-		</div>
+		<>
+		<CssBaseline/>
+		<Header/>
+		<Grid container spacing={3} style={{width:'100%'}}>
+		<Grid item xs={12} md={4}>
+		<Map/>
+		</Grid> 
+		<Grid item xs={12} md={8}>
+		<Map/>
+		</Grid> 
+		</Grid>
+		</>
 		);
 }
 
-export default App;
+export default App; 
